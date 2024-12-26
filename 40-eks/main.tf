2a5b56fa83d1b3a0db978a7d29894a5ce1b4fb85
prasad <prasad@gmail.com>
2024-12-26 14:32:30 +0530

    roboshop-infra

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -2,8 +2,7 @@ resource "aws_key_pair" "eks" {
   key_name   = "eks"
   # you can paste the public key directly like this
   #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
-  //public_key = file("~/.ssh/eks.pub")
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE3YDmEQLHxlo8xq9Yyed399qLGuYXwhFKtXes/Ph0cc pc@PrasadYadav"
+  public_key = file("~/.ssh/tools.pub")
   # ~ means windows home directory
 }
 
